aed859f7e6fb5a8068b4f6b372278598303d3eb9
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-04-07 05:23:47 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance < 400){
+    return 'This one is on me!';
+  } else if ((400 < distance)&&(distance<2000)){
+    return 'That will be twenty bucks.';
+  }
+  else if (2500 < distance){
+    return 'No can do.'
+  }
+  else {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city =="NYC"){
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == "generous"){
+    return 'Thank you so much.';
+  }
+  else if (tip == "not as generous"){
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
